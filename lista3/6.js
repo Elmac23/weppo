eba3391160c2cead2033cc77df8df07edcefe7f9
elmac23 <sakus04@wp.pl>
2025-11-02 15:55:33 +0100

    lista4

--- lista3/6.js
+++ lista3/6.js
@@ -22,6 +22,14 @@ function* fibGen() {
   }
 }
 
+var _it = fib();
+for (var _result; (_result = _it.next()), !_result.done; ) {
+  if (_result.value > 1000) break;
+  console.log(_result.value);
+}
+
+console.log("-----");
+
 for (var i of fibGen()) {
   console.log(i);
   if (i > 1000) break;
